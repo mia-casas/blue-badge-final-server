31176fc6962e0b142cd48d4748ee33856f8c7fe8
Mia Casas <miacasas@mias-mbp.attlocal.net>
2021-04-26 13:10:08 -0500

    heroku

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING)
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING, {
+    dialect: 'postgres',
+})
 module.exports = sequelize;
