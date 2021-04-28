73c44abce5a074db2fde4efb7568ffe88cf1bbad
Mia Casas <miacasas@mias-mbp.attlocal.net>
2021-04-28 13:32:47 -0500

    controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    userController: require("./userController"),
-    listController: require("./listController")
+    userController: require("./usercontroller"),
+    listController: require("./listcontroller")
 };
